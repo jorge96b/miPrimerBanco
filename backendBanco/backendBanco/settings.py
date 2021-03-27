d6837685d925f1486836e4916a3c01655ca79468
Jorge Bolaños <jorge_bolanos@coomeva.com.co>
2021-03-27 18:27:24 -0500

    se añade auth para login en proyecto

--- backendBanco/backendBanco/settings.py
+++ backendBanco/backendBanco/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_auth',
     'cuentas',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
--- backendBanco/backendBanco/urls.py
+++ backendBanco/backendBanco/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('api/', include('rest_auth.urls')),
     url(r'^', include(('cuentas.urls','cuentas'))),
 ]
 
